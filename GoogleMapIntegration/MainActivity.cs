9b8e06b9da50359f08b198fbc92032dd40b98d8b
M. Naveed Qamar <naveedqamar6500@gmail.com>
2022-09-09 22:58:47 +0500

    First Commit

---
+++ GoogleMapIntegration/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Gms.Maps;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using System;
+
+namespace GoogleMapIntegration
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+
+        private GoogleMap GMap;
+
+        [System.Obsolete]
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.activity_main);
+
+            SetUpMap();
+        }
+
+        [System.Obsolete]
+        private void SetUpMap()
+        {
+            if (GMap == null)
+            {
+                FragmentManager.FindFragmentById<MapFragment>(Resource.Id.googlemap).GetMapAsync(this);
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            this.GMap = googleMap;
+
+            GMap.UiSettings.ZoomControlsEnabled = true;
+
+            LatLng latlng = new LatLng(Convert.ToDouble(13.0291), Convert.ToDouble(80.2083));
+            CameraUpdate camera = CameraUpdateFactory.NewLatLngZoom(latlng, 15);
+            GMap.MoveCamera(camera);
+            MarkerOptions options = new MarkerOptions().SetPosition(latlng).SetTitle("Chennai");
+            GMap.AddMarker(options);
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ GoogleMapIntegration/obj/Debug/120/android/src/crc645849dfb629cce751/MainActivity.java
@@ -0,0 +1,76 @@
+package crc645849dfb629cce751;
+
+
+public class MainActivity
+	extends androidx.appcompat.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.maps.OnMapReadyCallback
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"n_onMapReady:(Lcom/google/android/gms/maps/GoogleMap;)V:GetOnMapReady_Lcom_google_android_gms_maps_GoogleMap_Handler:Android.Gms.Maps.IOnMapReadyCallbackInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"";
+		mono.android.Runtime.register ("GoogleMapIntegration.MainActivity, GoogleMapIntegration", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("GoogleMapIntegration.MainActivity, GoogleMapIntegration", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("GoogleMapIntegration.MainActivity, GoogleMapIntegration", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+
+	public void onMapReady (com.google.android.gms.maps.GoogleMap p0)
+	{
+		n_onMapReady (p0);
+	}
+
+	private native void n_onMapReady (com.google.android.gms.maps.GoogleMap p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
